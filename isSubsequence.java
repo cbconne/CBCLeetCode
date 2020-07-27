32c910a11be8177d07368a5a4cbffc53daba4db9
CBC <chenbc98@163.com>
2020-07-27 19:59:21 +0800

    idea test

---
+++ isSubsequence.java
@@ -0,0 +1,8 @@
+public class isSubsequence {
+    public static void main(String[] args) {
+        System.out.println(is_Subsequence("abc","ahbgdc"));
+    }
+    public static boolean is_Subsequence(String s, String t) {
+        return true;
+    }
+}
