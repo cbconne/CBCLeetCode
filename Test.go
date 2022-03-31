4e6a9a3e31380289581b9a59fa9883609b9034de
CBC <770663964@qq.com>
2022-03-31 21:53:15 +0800

    2022.3.31

--- Test.go
+++ Test.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-	k := 1
-	arrival := []int{1}
-	load := []int{1}
-	fmt.Printf("study2022_3.BusiestServers(k, arrival, load): %v\n", study2022_3.BusiestServers(k, arrival, load))
+	left := 47
+	right := 85
+	fmt.Printf("study2022_3.SelfDividingNumbers(left, right): %v\n", study2022_3.SelfDividingNumbers(left, right))
 }
