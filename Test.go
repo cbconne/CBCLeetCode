57a2c255a7037179378f20bee4d3d89bf98a559a
CBC <770663964@qq.com>
2023-05-29 09:40:52 +0800

    2023.05.29

--- Test.go
+++ Test.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	study2023_5 "cbcLeetCode/2023_5"
+	Interview_150 "cbcLeetCode/Interview_150"
+	"fmt"
 )
 
 func main() {
-	study2023_5.Test()
+	// 每日一题
+	fmt.Println("每日一题：")
+	study2023_5.AverageValueTest()
+
+	// 面试题
+	fmt.Println("面试题：")
+	Interview_150.MergeTest()
 }
