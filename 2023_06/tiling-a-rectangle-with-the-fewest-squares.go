40542b059e26f33731f489f250a6e5ca45593626
CBC <770663964@qq.com>
2023-06-08 12:30:33 +0800

    2023.06.08

---
+++ 2023_06/tiling-a-rectangle-with-the-fewest-squares.go
@@ -0,0 +1,14 @@
+package study2023_06
+
+func tilingRectangle(n int, m int) int {
+	// hard problem
+	// 告辞！
+	return 0
+}
+
+func TilingRectangleTest() {
+	// 输入：n = 2, m = 3
+	n := 2
+	m := 3
+	println(tilingRectangle(n, m))
+}
