53147075c16858db1c9948c5b3b7b894a8a37553
CBC <770663964@qq.com>
2023-06-16 08:41:20 +0800

    2023.06.16

--- leetcode_daily_problem.py
+++ leetcode_daily_problem.py
@@ -36,7 +36,11 @@ def get_daily_problem_title():
 def get_daily_problem_info(leetcodeTitle):
     # url = base_url + "/problems/" + leetcodeTitle
     response = requests.post(base_url + "/graphql",
-                             json={"operationName": "questionData", "variables": {"titleSlug": leetcodeTitle},
+                             json={"operationName": "questionData",
+                                   "variables":
+                                   {
+                                       "titleSlug": leetcodeTitle
+                                   },
                                    "query": "query questionData($titleSlug: String!) {  question(titleSlug: $titleSlug) {    questionId    questionFrontendId    boundTopicId    title    titleSlug    content    translatedTitle    translatedContent    isPaidOnly    difficulty    likes    dislikes    isLiked    similarQuestions    contributors {      username      profileUrl      avatarUrl      __typename    }    langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }    companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }    stats    hints    solution {      id      canSeeDetail      __typename    }    status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo    book {      id      bookName      pressName      source      shortDescription      fullDescription      bookImgUrl      pressImgUrl      productUrl      __typename    }    isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  }}"})
     # 转化成json格式
     jsonText = json.loads(response.text).get('data').get("question")
@@ -53,8 +57,9 @@ def create_folder():
 
 def create_file(file_name, folder_name, problem_info, language='Go'):
     # 获取代码模板（codeSnippets == language）, get('code')
-    codeSnippets = list(filter(lambda x: x.get('lang') ==
-                               language, problem_info.get('codeSnippets')))[0].get('code')
+    codeSnippets = list(filter(lambda x: x.get('lang') == language,
+                               problem_info.get('codeSnippets'))
+                        )[0].get('code')
     with open(file_name, 'w') as f:
         # package, name = study{yyyy}_{mm}
         f.write('package study' + folder_name + '\n')
