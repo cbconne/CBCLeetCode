54590deb960bb8065be44b35c4be873df4d29817
u3666 <chenbicong@smec-cn.com>
2024-01-30 14:33:53 +0800

    2024.01.30

--- Test.go
+++ Test.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	study2023_06 "cbcLeetCode/2023_06"
 	Interview_150 "cbcLeetCode/Interview_150"
 	"fmt"
 )
 
 func main() {
-	// 每日一题
-	fmt.Println("每日一题：")
-	study2023_06.MaxSumDivThreeTest()
+	// // 每日一题
+	// fmt.Println("每日一题：")
+	// study2023_06.MaxSumDivThreeTest()
 
 	// 面试题
 	fmt.Println("面试题：")
-	Interview_150.CanCompleteCircuitTest()
+	Interview_150.CandyTest()
 }
